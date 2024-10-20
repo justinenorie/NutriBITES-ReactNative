d02f538cd860c46947fd70c72a6b323a1e98ad6d
justinenorie <justinedelacruz97@gmail.com>
2024-10-20 11:48:58 +0800

    Added Animation through Navigating Screens

--- navigation/mainNavigation.jsx
+++ navigation/mainNavigation.jsx
@@ -1,5 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import HomePage from '../screens/HomePage';
+import AboutUs from '../screens/AboutUs';
 import LoginScreen from '../screens/LoginPages';
 import SignUpScreen from '../screens/SignUpPage';
 
@@ -8,10 +10,20 @@ const Stack = createNativeStackNavigator();
 export default function Navigation() {
     return (
         <NavigationContainer independent={true}>
-            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false,
+                    animation: 'fade_from_bottom',
+                    animationTypeForReplace: 'push',
+                }}
+                initialRouteName="Home"
+            >
+                <Stack.Screen name="Home" component={HomePage} />
+                <Stack.Screen name="About" component={AboutUs} />
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="SignUp" component={SignUpScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
 }
+
