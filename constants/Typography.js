d082a9a116b5f3ca83804945f4f895d9c8fdedf3
justinenorie <justinedelacruz97@gmail.com>
2024-10-20 12:24:25 +0800

    Typography styling

---
+++ constants/Typography.js
@@ -0,0 +1,11 @@
+import colors from './Colors';
+
+const fonts = {
+    logoName: { fontFamily: 'MochiyPopOne-Regular', fontSize: 30 , color: '#1F2240'},
+    title: { fontFamily: 'Montserrat-Regular', fontSize: 30 },
+    h1: { fontFamily: 'Ubuntu-Regular', fontSize: 20 },
+    h2: { fontFamily: 'Ubuntu-Regular', fontSize: 15 },
+    small: { fontFamily: 'Ubuntu-Regular', fontSize: 10 },
+};
+export default fonts;
+
