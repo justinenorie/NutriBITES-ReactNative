ce7ff50e42176ede99545fa044df4a1d22f77627
justinenorie <justinedelacruz97@gmail.com>
2024-10-22 07:42:20 +0800

    Changes in membersData

---
+++ data/membersData.js
@@ -0,0 +1,53 @@
+const membersData = [
+    {
+        id: 1,
+        name: "Justine Norie B. Dela Cruz",
+        assign: "Frontend Developer, UI/UX Designer",
+        picture: "noriePic",
+        description: "I apply of what I've learned about UI/UX design principles, such as element spacing, typography, and content block usage, I use Figma to create a design for our system. After designing, I used React Native to build a working mobile application, applying the basic concepts of the framework. I implemented navigation for accessing different pages, styled the application, and created various interactive pages. I also practiced version control by uploading the project to GitHub and followed best practices for file organization, ensuring the code is easy for others to understand."
+    },
+    {
+        id: 2,
+        name: "Brando Baja",
+        assign: "User Persona, Styling",
+        picture: "brandoPic",
+        description: "I gained knowledge of UI/UX design principles such as element spacing, typography, and content block usage, and utilized these concepts to create a design for our system using Figma. Additionally, I developed a user persona to facilitate better collaboration with my team, ensuring that our design aligns with the needs of our target users."
+    },
+    {
+        id: 3,
+        name: "Harlenkens Q. Sanchez",
+        assign: "UI/UX Designer, Color Styling, User Persona, UI/UX Designer, Color Styling, User Persona, UI/UX Designer, Color Styling, User Persona",
+        picture: "harlenPic",
+        description: "Aye"
+    },
+    {
+        id: 4,
+        name: "Noverjohn Rivas",
+        assign: "Typography Styling",
+        picture: "rivasPic",
+        description: "Aye"
+    },
+    {
+        id: 5,
+        name: "Rholine Jay Main",
+        assign: "Space Styling",
+        picture: "rholinePic",
+        description: "In my contribution, I applied spacing, using padding to determine the precise distance between element borders and their contents. And I utilized margin to create space around the element's border, controlling the distance between separate elements on the page. Calculating the exact sizes for both padding and margin is crucial for achieving proper alignment."
+    },
+    {
+        id: 6,
+        name: "Ma. Melissa Maracha Onipa",
+        assign: "Color Theory Styling",
+        picture: "marachaPic",
+        description: "In Figma, I designed a color palette that includes primary, secondary, and accent colors using real-time color feedback. This approach allows for immediate adjustments and visualizations, ensuring that the colors work harmoniously together. The real-time aspect enhances the design process by making it easier to see how colors interact in different contexts."
+    },
+    {
+        id: 7,
+        name: "Frocelie Juan",
+        assign: "60/30/10 Rule Styling",
+        picture: "froceliePic",
+        description: "The 30/60/10 rule is a design principle I adhere to, which recommends allocating 30% of a dominant color, 60% of a secondary color, and 10% of an accent color to achieve a balanced composition. In Figma, I selected a primary color palette that aligns with the brand identity. I applied the dominant color to key elements, while the secondary color was used to enhance text and buttons. The accent color was utilized sparingly for highlights and calls to action, ensuring it stands out without overwhelming the overall design."
+    }
+];
+
+export default membersData;
