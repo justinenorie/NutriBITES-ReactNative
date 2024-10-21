a772d069f4220c3ac430d773a8e28fd6338e11a9
justinenorie <justinedelacruz97@gmail.com>
2024-10-21 12:32:43 +0800

    Typography changes

--- App.jsx
+++ App.jsx
@@ -5,8 +5,8 @@ import { ActivityIndicator, View } from 'react-native';
 
 export default function App() {
     const [fontsLoaded] = useFonts({
-        'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
-        'Ubuntu-Regular': require('./assets/fonts/Ubuntu-Regular.ttf'),
+        'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+        'Quicksand-Regular': require('./assets/fonts/Quicksand-Regular.ttf'),
         'MochiyPopOne-Regular' : require('./assets/fonts/MochiyPopOne-Regular.ttf')
     });
 
--- constants/Typography.js
+++ constants/Typography.js
@@ -1,13 +1,9 @@
 import colors from './Colors';
-
 const fonts = {
-    logoName: { fontFamily: 'MochiyPopOne-Regular', fontSize: 30 , color: '#1F2240'},
-    title: { fontFamily: 'Montserrat-Regular', fontSize: 30, color: colors.TEXTblack },
-    h1: { fontFamily: 'Ubuntu-Regular', fontSize: 20, fontWeight: 'bold', color: colors.TEXTblack },
-    h2: { fontFamily: 'Ubuntu-Regular', fontSize: 15, color: colors.TEXTblack },
-    small: { fontFamily: 'Ubuntu-Regular', fontSize: 13, color: colors.TEXTblack },
-
-    
+    logoName: { fontFamily: 'Poppins-Regular', fontWeight: "bold", color: '#1F2240'},
+    title: { fontFamily: 'MochiyPopOne-Regular', fontSize: 30, color: colors.TEXTblack },
+    h1: { fontFamily: 'Quicksand-Regular', fontSize: 20, fontWeight: 'bold', color: colors.TEXTblack },
+    h2: { fontFamily: 'Quicksand-Regular', fontSize: 15, color: colors.TEXTblack },
+    small: { fontFamily: 'Quicksand-Regular', fontSize: 13, color: colors.TEXTblack },
 };
 export default fonts;
-
