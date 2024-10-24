c99d8df340e68e6b7fa11321ea2a5aebbd6aee5c
justinenorie <justinedelacruz97@gmail.com>
2024-10-24 09:03:52 +0800

    darkmode shades

--- constants/Colors.js
+++ constants/Colors.js
@@ -2,8 +2,12 @@ const colors = {
     BACKGROUND: '#FAF9F6',
     PRIMARY: '#DEB595',
     ACCENT: '#D28A8A',
-    TEXTwhite: '#EDEDED',
-    TEXTblack: '#050315',
     SECONDARY: '#1F2240',
+    TEXTwhite: '#EDEDED',
+    TEXTblack: '#202020',
+
+    //DarkMode shades
+    DARKBACKGROUND: '#050315',
+    DARKSECONDARY: '#C4C3D5',
 };
 export default colors;
