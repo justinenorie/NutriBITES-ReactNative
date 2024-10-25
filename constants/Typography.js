99e587d858b302e6ab258c93ed95ab32ed566e78
justinenorie <justinedelacruz97@gmail.com>
2024-10-25 09:06:49 +0800

    added font styling

--- constants/Typography.js
+++ constants/Typography.js
@@ -2,12 +2,14 @@ import colors from './Colors';
 const fonts = {
     logoName: { fontFamily: 'Poppins-Regular', fontWeight: "bold", color: '#1F2240'},
     title: { fontFamily: 'MochiyPopOne-Regular', fontSize: 30, color: colors.TEXTblack },
+    subtitle: { fontFamily: 'Oswald-Bold', fontSize: 25, color: colors.TEXTblack },
     h1: { fontFamily: 'Quicksand-Regular', fontSize: 20,  color: colors.TEXTblack },
     h2: { fontFamily: 'Quicksand-Regular', fontSize: 18, color: colors.TEXTblack },
     small: { fontFamily: 'Quicksand-Regular', fontSize: 15, color: colors.TEXTblack },
 
     
     titleWhite: { fontFamily: 'MochiyPopOne-Regular', fontSize: 30, color: colors.TEXTwhite },
+    subtitleWhite: { fontFamily: 'Oswald-Bold', fontSize: 25, color: colors.TEXTwhite },
     h1White: { fontFamily: 'Quicksand-Regular', fontSize: 20,  color: colors.TEXTwhite },
     h2White: { fontFamily: 'Quicksand-Regular', fontSize: 18, color: colors.TEXTwhite },
     smallWhite: { fontFamily: 'Quicksand-Regular', fontSize: 15, color: colors.TEXTwhite },
