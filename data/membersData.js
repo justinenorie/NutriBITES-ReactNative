a92a3cb68556ce31773d568e2ca8e16f40557140
Justine Norie <116890169+justinenorie@users.noreply.github.com>
2024-10-26 10:28:51 +0800

    Update Data Description for Harlenkens

--- data/membersData.js
+++ data/membersData.js
@@ -4,21 +4,21 @@ const membersData = [
         name: "Justine Norie B. Dela Cruz",
         assign: "Frontend Developer, UI/UX Designer",
         picture: "noriePic",
-        description: "I apply of what I've learned about UI/UX design principles, such as element spacing, typography, and content block usage, I use Figma to create a design for our system. After designing, I used React Native to build a working mobile application, applying the basic concepts of the framework. I implemented navigation for accessing different pages, styled the application, and created various interactive pages. I also practiced version control by uploading the project to GitHub and followed best practices for file organization, ensuring the code is easy for others to understand."
+        description: "As a Frontend Developer and one of the UI/UX Designers, I apply design principles like spacing, typography, and content structure using Figma to create our system design layout. I use React Native one of the framework in JavaScript, to bring the design to life, building a functional mobile application with navigation, styling, and interactive pages. I manage version control through GitHub and ensuring the organized code."
     },
     {
         id: 2,
         name: "Brando Baja",
-        assign: "User Persona, Styling",
+        assign: "User Persona, UI/UX Designer",
         picture: "brandoPic",
         description: "I gained knowledge of UI/UX design principles such as element spacing, typography, and content block usage, and utilized these concepts to create a design for our system using Figma. Additionally, I developed a user persona to facilitate better collaboration with my team, ensuring that our design aligns with the needs of our target users."
     },
     {
         id: 3,
         name: "Harlenkens Q. Sanchez",
-        assign: "UI/UX Designer, Color Styling, User Persona, UI/UX Designer, Color Styling, User Persona, UI/UX Designer, Color Styling, User Persona",
+        assign: "UI/UX Designer, User Persona",
         picture: "harlenPic",
-        description: "My main role that has given to me is called Typography, being a member in this group, I learned how to find the right font, style, spacing, and adjust is quite challenging because finding the correct way on how to align the head, subhead and title."
+        description: "As one of the UI/UX Designers, I use user personas to deeply understand our audience's needs, preferences, and goals. This approach allows me to craft intuitive and meaningful designs that resonate with users, ensuring our system design are not only visually appealing but also functional in displaying contents and user-centered."
     },
     {
         id: 4,
@@ -49,5 +49,4 @@ const membersData = [
         description: "The 30/60/10 rule is a design principle I adhere to, which recommends allocating 30% of a dominant color, 60% of a secondary color, and 10% of an accent color to achieve a balanced composition. In Figma, I selected a primary color palette that aligns with the brand identity. I applied the dominant color to key elements, while the secondary color was used to enhance text and buttons. The accent color was utilized sparingly for highlights and calls to action, ensuring it stands out without overwhelming the overall design."
     }
 ];
-
 export default membersData;
